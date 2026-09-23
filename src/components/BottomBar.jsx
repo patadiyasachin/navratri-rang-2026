8b2e7faffcee9006d9b26f3a5ed240bb1f577c5f
Sachin Patadiya <22010101142@darshan.ac.in>
2026-09-23 19:47:36 +0530

    add address

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import { contactNumbers } from "../data/content";
 
 export default function BottomBar() {
+  const officeAddress = "૧-પારસમણી કોમ્પ્લેક્ષ, રાજમંદિર કૉલ્ડ્રીંક્સની બાજુમાં, પેલેસ રોડ, રાજકોટ.";
+
   return (
     <div className="bottom-bar">
-      <a className="call-button" href={`tel:+91${contactNumbers[0].replace(/\s/g, "")}`}>
-        BOOK YOUR PASS
-      </a>
-      <div className="phone-list">
-        {contactNumbers.map((number) => (
-          <a key={number} href={`tel:+91${number.replace(/\s/g, "")}`}>
-            +91 {number}
-          </a>
-        ))}
+      <div className="bottom-bar-row top-row">
+        <a className="call-button" href={`tel:+91${contactNumbers[0].replace(/\s/g, "")}`}>
+          BOOK YOUR PASS
+        </a>
+
+        <div className="phone-list">
+          {contactNumbers.map((number) => (
+            <a key={number} href={`tel:+91${number.replace(/\s/g, "")}`}>
+              +91 {number}
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <div className="bottom-bar-row address-row">
+        <span className="address-line">{officeAddress}</span>
       </div>
     </div>
   );
